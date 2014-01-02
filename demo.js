95c4725b7859d2e0ab8d7bcfff1d6424bb128ab1
deoxxa <deoxxa@fknsrs.biz>
2014-01-02 14:35:10 +1100

    refactoring! partway!

--- demo.js
+++ demo.js
@@ -1,73 +1,180 @@
 #!/usr/bin/env node
 
-var net = require("net"),
+var bencode = require("bencode-stream"),
+    Bitfield = require("opaque-bitfield"),
+    crypto = require("crypto"),
+    events = require("events"),
+    fs = require("fs"),
+    net = require("net"),
     BitTorrent = require("./bittorrent");
 
-var stream = new BitTorrent.Protocol.TCP.Stream();
-var socket = net.createConnection(51413, "127.0.0.1");
+var TorrentSession = function TorrentSession(options) {
+  options = options || {};
 
-socket.pipe(stream);
-stream.pipe(socket);
+  events.EventEmitter.call(this, options);
 
-stream.on("handshake", function(handshake) {
-  console.log("handshake", handshake);
+  this.infoHash = options.infoHash || null;
+  this.torrent = options.torrent || null;
+  this.length = options.length || null;
+  this.pieceCount = options.pieceCount || null;
+  this.pieceLength = options.pieceLength || null;
+  this.bitfield = options.bitfield || null;
+  this.storage = options.storage || null;
+
+  this.connections = [];
+};
+TorrentSession.prototype = Object.create(events.EventEmitter.prototype, {constructor: {value: TorrentSession}});
+
+TorrentSession.prototype.loadFromObject = function loadFromObject(torrent, done) {
+  if (typeof torrent.info !== "object" || torrent.info === null) {
+    return done(Error("info key is missing"));
+  }
+
+  if (!torrent.info.pieces || !Buffer.isBuffer(torrent.info.pieces)) {
+    return done(Error("pieces key is missing or invalid"));
+  }
+
+  this.torrent = torrent;
+  this.length = torrent.info.length;
+  this.pieceCount = torrent.info.pieces.length / 20;
+  this.pieceLength = torrent.info["piece length"];
+  this.bitfield = new Bitfield(Math.ceil(this.pieceCount / 8));
+
+  this.pieceHashes = [];
+  for (var i=0;i<this.pieceCount;++i) {
+    this.pieceHashes[i] = torrent.info.pieces.slice(i * 20, i * 20 + 20);
+  }
+
+  var liberator = new bencode.Liberator(),
+      encoder = new bencode.Encoder();
+
+  var self = this;
+  liberator.on("error", done).pipe(encoder).on("error", done).pipe(crypto.createHash("sha1")).on("error", done).on("data", function(infoHash) {
+    self.setInfoHash(infoHash);
+
+    self.emit("torrent", torrent);
+
+    return done();
+  });
+
+  liberator.end(torrent.info);
+};
+
+TorrentSession.prototype.setInfoHash = function setInfoHash(infoHash) {
+  this.infoHash = infoHash;
+
+  this.emit("infoHash", infoHash);
+};
+
+TorrentSession.prototype.setStorage = function setStorage(storage) {
+  this.storage = storage;
+};
+
+TorrentSession.prototype.connectTo = function connectTo(options, done) {
+  var _done = done;
+
+  var calledDone = false;
+  done = function done() {
+    if (calledDone) { return; } else { calledDone = true; }
+    return _done.apply(null, arguments);
+  };
+
+  var self = this;
+
+  var connection = new BitTorrent.Protocol.TCP.Connection({
+    infoHash: this.infoHash || null,
+    bitfield: this.bitfield ? new Bitfield(this.bitfield.toBuffer()) : null,
+  });
+
+  var socket = net.connect(51413, "127.0.0.1");
+
+  socket.on("error", done).pipe(connection).on("error", done).pipe(socket);
+
+  connection.handshake();
+
+  connection.on("infoHash", function(infoHash) {
+    console.log("infoHash", infoHash);
+  });
+
+  connection.on("peerId", function(peerId) {
+    console.log("peerId", peerId);
+  });
+
+  connection.on("bitfield", function(bitfield) {
+    console.log("bitfield", bitfield);
+
+    return done(null, connection);
+
+    connection.bitfield().interested();
+  });
+
+  connection.on("choked", function() {
+    console.log("choked");
+  });
+
+  connection.on("unchoked", function() {
+    console.log("unchoked");
+  });
+
+  connection.on("interested", function() {
+    console.log("interested");
+  });
+
+  connection.on("notInterested", function() {
+    console.log("notInterested");
+  });
+
+  connection.on("piece", function(piece) {
+    console.log(piece);
+  });
+};
+
+var session = new TorrentSession();
+
+session.on("torrent", function(torrent) {
+  var storage = new BitTorrent.Storage.File({
+    filename: "./something",
+    length: session.length,
+    pieceHashes: session.pieceHashes,
+    pieceCount: session.pieceCount,
+    pieceLength: session.pieceLength,
+  });
+
+  storage.initialise(function(err) {
+    if (err) {
+      return console.warn(err);
+    }
+
+    storage.fullHashCheck(function(err) {
+      if (err) {
+        return console.warn(err);
+      }
+
+      session.setStorage(storage);
+    });
+  });
 });
 
-stream.on("peer_id", function(peer_id) {
-  console.log("peer_id", peer_id);
-});
+var decoder = new bencode.Decoder(),
+    accumulator = new bencode.Accumulator(),
+    objectifier = new bencode.Objectifier();
 
-stream.on("data", function(data) {
-//  console.log("data", data);
-});
+fs.createReadStream("./random.torrent").pipe(decoder).pipe(accumulator).pipe(objectifier).on("data", function(torrent) {
+  session.loadFromObject(torrent, function(err) {
+    if (err) {
+      return console.warn(err);
+    }
 
-stream.on("packet", function(packet) {
-  console.log("packet", packet);
-});
+    /*
+    session.connectTo({host: "127.0.0.1", port: 51413}, function(err, connection) {
+      if (err) {
+        return console.warn(err);
+      }
 
-stream.on("keep_alive", function() {
-  console.log("keep_alive");
-});
+      console.log("connected");
+    });
+    */
 
-stream.on("choked", function() {
-  console.log("choked");
+    console.log(session);
+  });
 });
-
-stream.on("unchoked", function() {
-  console.log("unchoked");
-});
-
-stream.on("interested", function() {
-  console.log("interested");
-});
-
-stream.on("uninterested", function() {
-  console.log("uninterested");
-});
-
-stream.on("have", function(index) {
-  console.log("have", index);
-});
-
-stream.on("bitfield", function(bitfield) {
-  console.log("bitfield", bitfield);
-});
-
-stream.on("request", function(request) {
-  console.log("request", request);
-});
-
-stream.on("block", function(block) {
-  console.log("block", block);
-});
-
-stream.on("cancel", function(request) {
-  console.log("cancel", request);
-});
-
-stream.on("port", function(port) {
-  console.log("port", port);
-});
-
-stream.handshake("BitTorrent protocol", Buffer([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]), "76fa017cea2cf63013b21c75f55fbd1d4de7abbb");
-stream.peer_id("BT.JS-00000000000000");
